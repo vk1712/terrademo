29b829cd79f00c71156b477d1c98c5d141e0eb2b
Vishal Kumar <33510648+vk1712@users.noreply.github.com>
2026-01-29 04:15:03 +0000

    initial push

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "geometric-shift-485514-b5"
+  region  = "us-central1"
+}
