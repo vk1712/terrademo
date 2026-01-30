ed08ae5c62f5a1c3f06f6a20c98f0ac508c3387c
Vishal Kumar <33510648+vk1712@users.noreply.github.com>
2026-01-30 17:21:47 +0000

    terraform initial push

--- main.tf
+++ main.tf
@@ -2,12 +2,34 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "7.16.0"
+      version = "7.17.0"
     }
   }
 }
 
 provider "google" {
-  project = "geometric-shift-485514-b5"
-  region  = "us-central1"
+  credentials = file(var.credentials)
+  project = var.project
+  region  = var.region
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "terraform-demo-485816-demo-bucket"
+  location      = var.location
+  force_destroy = true
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
